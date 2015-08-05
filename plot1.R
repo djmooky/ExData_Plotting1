a1f9275c0664cd7ad24af9ab104d1d0b87ccb8d8
djmooky <dj.mooky@gmail.com>
2015-08-04 23:21:57 -0400

    removed divide by 1K in plot1

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ if (!exists("theData"))
 	theData <- getData()
 
 png("plot1.png", width=480, height=480)
-theData$Global_active_power <- theData$Global_active_power / 1000
+#theData$Global_active_power <- theData$Global_active_power / 1000
 hist(theData$Global_active_power, col="red",
 	main="Global Active Power", xlab="Global Active Power (Kilowatts)")
 dev.off()
